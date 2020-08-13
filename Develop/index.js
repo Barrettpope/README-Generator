bf531a57fa9dc01444db68b000cef2984f3aa1f3
Barrett Pope <barrettpope94@gmail.com>
2020-08-13 09:50:32 -0400

    prompt function added and moodified READme markup

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,74 @@
-// array of questions for user
-const questions = [
+// Requiring node modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-];
+
+// array of questions for user
+function promptUser() {
+    return inquirer.prompt([{
+            type: "input",
+            message: "Please enter the title of your application:",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Please enter a brief description of your application:",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Please enter installation instructions for your application:",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "Please enter usage information for your application:",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Please enter contribution guidelines for your application:",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Please enter test instructions for your application:",
+            name: "test"
+        },
+        {
+            type: "list",
+            message: "Please select a license for your application:",
+            choices: ["MIT", "Open Software License 3.0", "Microsoft Public License", "Eclipse Public License 1.0"],
+            name: "license"
+        },
+        {
+            type: "input",
+            message: "Please enter your GitHub username:",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "Please enter your email address:",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter installation instructions for your project:",
+            name: "confirm"
+        }
+    ])
+};
+promptUser()
+
+    .then(function (response) {
+
+    })
+
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,17 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // `#${data.title}`;
+  // `#Table of Contents \n ${data.toc}`;
+  // `#Description \n ${data.description}`;
+  // `#Installation \n ${data.installation}`;
+  // `#Usage \n ${data.usage}`;
+  // `#Tests \n ${data.test}`;
+  // `#Questions \n ${data.installation}`;
+  // `#Contributing \n ${data.contribution}`;
+  // `#License \n ${data.license}`;
 
-`;
+  // return
+
 }
-
+generateMarkdown()
 module.exports = generateMarkdown;
